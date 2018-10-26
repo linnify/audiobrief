4108a83b7279508440e711b99b6f19936f7310b5
darius <dariusmihneabogdan@gmail.com>
2018-10-26 21:02:37 +0300

    make share a component and add topics on first login

---
+++ src/app/shared/components/share/share.component.ts
@@ -0,0 +1,58 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {environment} from '../../../../environments/environment';
+import {NewsEntry} from '../../../news/types/news-entry';
+
+@Component({
+  selector: 'audiobrief-share',
+  template: `
+    <button *ngIf="icon" mat-icon-button class="main-button" [matMenuTriggerFor]="shareMenu"
+            (click)="$event.stopPropagation()">
+      <mat-icon class="big-icon">share</mat-icon>
+    </button>
+    <div  *ngIf="!icon" class="mat-title" [matMenuTriggerFor]="shareMenu">
+      SHARE
+    </div>
+    
+    <mat-menu #shareMenu="matMenu">
+
+      <button mat-menu-item (click)="onOpenUrl('https://www.facebook.com/sharer/sharer.php?u=')">
+        <mat-icon svgIcon="facebook"></mat-icon>
+        <span>Facebook</span>
+      </button>
+      <button mat-menu-item (click)="onOpenUrl('https://twitter.com/home?status=')">
+        <mat-icon svgIcon="twitter"></mat-icon>
+        <span>Twitter</span>
+      </button>
+      <button mat-menu-item (click)="onOpenUrl('https://www.linkedin.com/shareArticle?mini=true&url=')">
+        <mat-icon svgIcon="linkedin"></mat-icon>
+        <span>Linkedin</span>
+      </button>
+      <button mat-menu-item [text-copy]="newsEntry.url">
+        <mat-icon svgIcon="copy"></mat-icon>
+        <span>Copy link</span>
+      </button>
+    </mat-menu>
+  `,
+  styleUrls: ['./share.component.scss']
+})
+export class ShareComponent implements OnInit {
+  @Input() newsEntry: NewsEntry;
+  @Input() icon: boolean;
+
+  @Output() openUrl: EventEmitter<{url: string, config: any}> = new EventEmitter<{url: string, config: any}>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onOpenUrl(sharerUrl: string): void {
+    const url: string = `${sharerUrl}${environment.host}app/news/${this.newsEntry.id}`;
+    const config: any = {
+      title: this.newsEntry.title,
+      description: 'Small description',
+      url: `${environment.host}app/news/${this.newsEntry.id}`
+    };
+    this.openUrl.emit({url, config});
+  }
+}
